996ad72346e9ff0e14414d453f5162705886875f
Austin DeLauney <adelauney96@outlook.com>
2017-01-15 17:29:31 -0500

    Error handling for program execution added

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,24 @@ use std::process::Command;
 
 fn main()
 {
-	loop
+	let mut exit_status;
+    loop
     {
+        print!("Shelly$ ");
+        io::stdout().flush().unwrap();
+
         let mut command = String::new();
+        
         match io::stdin().read_line(&mut command) {
             Err(error) => { println!("Error reading line"); continue; },
             _ => (),
         }
         let command = command.trim();
 
-        let mut child = Command::new(command).spawn()
-			.expect("Error");
-
-		let ecode = child.wait()
-			.expect("Failed to wait on child");
+        match Command::new(command).spawn() {
+            Err(error) => { println!("Error executing, {}", error); continue;},
+            Ok(mut child) => { exit_status = child.wait(); },
+        }
 
 	}
 		
