10ef86d916674261cec078979448095bf20bdb34
laundry_96 <adelauney96@outlook.com>
2017-03-20 15:58:11 -0400

    Shell now waits for child to exit.

--- src/process/process.rs
+++ src/process/process.rs
@@ -1,11 +1,11 @@
 use std::process::{Command, Child};
 use std::string;
+use std::result;
 pub fn exec(command: Vec<&str>) {
 	println!("{:?}", command);
 	let mut child = Command::new(command[0]).spawn();
-	let act_child = match child {
-		Ok(a) => a,
-		Err(b)  => println!("{}",b),
-	};
+	if(child.is_ok()){
+		child.unwrap().wait();
+	}
 	//child.wait().expect("command wasn't running?");
 }
