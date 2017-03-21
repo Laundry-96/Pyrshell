1f8cd401cdb1b275d779c6f245b898beca8cc325
laundry_96 <adelauney96@outlook.com>
2017-03-20 22:27:17 -0400

    Prints an error correctly if there is an error.

--- src/process/process.rs
+++ src/process/process.rs
@@ -4,8 +4,13 @@ use std::result;
 pub fn exec(command: Vec<&str>) {
 	println!("{:?}", command);
 	let mut child = Command::new(command[0]).spawn();
+
+		
 	if(child.is_ok()){
 		child.unwrap().wait();
+	}
+	else{
+		println!("{}", child.err().unwrap());
 	}
 	//child.wait().expect("command wasn't running?");
 }
